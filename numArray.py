1f2f6c6d105b6ef3d04666c01bc51869a9030727
Yashas_J <yashasjraj@gmail.com>
2024-01-04 16:39:23 +0530

    numpy array

---
+++ numArray.py
@@ -0,0 +1,49 @@
+from numpy import *
+
+# ways of creating array in numpy
+arr = array([[10, 20, 30], [1, 2, 3], [4, 5, 6]])
+
+# print(arr)
+
+# arr2 = linspace(1, 15, 10)
+
+# print(arr2)
+
+arr1 = zeros(5, int)
+
+# print(arr1)
+
+arr3 = ones(5, int)
+
+# print(arr3)
+
+arr1 = array([10, 20, 30])
+
+arr1 = append(arr1, 30)
+
+# print(arr1)
+
+arr1 = insert(arr1, 1, 5)
+
+# print(arr1)
+
+arr1.put(0, 15)
+
+# print(arr1)
+
+# print(arr1)
+#
+# arr5 = arr1.copy()
+#
+# print(id(arr1))
+#
+# print(id(arr5))
+
+
+ar = array([[1, 2, 3], [3, 4, 5], [6, 7, 8]])
+
+m = matrix(ar)
+
+print(m)
+
+print(m)
